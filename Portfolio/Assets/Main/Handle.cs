04e4df9ab464b917eaa71967e538460652d130e6
ssb5783 <110151488+ssb5783@users.noreply.github.com>
2023-03-12 14:24:45 +0900

    project

---
+++ Portfolio/Assets/Main/Handle.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Handle : MonoBehaviour
+{
+    //핸들을 계속 좌우로 움직이게 하고싶다.
+    //필요속성 : 핸들 배열, 이동 속도
+    public Transform[] Handles = new Transform[87]; //핸들 배열
+    public float speed = 0;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
